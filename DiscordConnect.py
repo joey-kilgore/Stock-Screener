6685daa3f7c9fe14ed67615e32a8a5b6379b7c64
Joey Kilgore <joey.a.kilgore@gmail.com>
2019-07-06 19:41:49 -0400

    added commands

--- DiscordConnect.py
+++ DiscordConnect.py
@@ -1,7 +1,11 @@
 # Work with Python 3.6
 import discord
+import json
+import WebScrape
 
-TOKEN = ''
+with open('Secret.json') as json_file:  
+    data = json.load(json_file)
+    TOKEN = data['discordKey']
 
 client = discord.Client()
 
@@ -13,6 +17,56 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send(msg)
+
+    elif message.content.startswith('!recordanewmixtape'):
+        msg = 'No {0.author.mention}, you do it'.format(message)
+        await message.channel.send(msg)
+
+    elif message.content.startswith('!tickers'):
+        cmd = message.content
+        cmd = cmd[9:]
+        print('tickers command ticker: ' + cmd)
+        try:
+            tickers = WebScrape.printTickers(cmd)
+            msg = ''
+            for tick in tickers:
+                msg = msg + tick + '\n'
+        except:
+            msg = 'Not a valid option\nLook at !listOptions'
+            
+        await message.channel.send(msg)
+
+    elif message.content.startswith('!setOption'):
+        cmd = message.content
+        cmdParts = cmd.split()
+        WebScrape.setOption(cmdParts[1], cmdParts[2])
+        msg = '{0.author.mention} new option added '.format(message) + cmdParts[2]
+        await message.channel.send(msg)
+
+    elif message.content.startswith('!removeOption'):
+        cmd = message.content
+        cmdParts = cmd.split()
+        WebScrape.removeOption(cmdParts[1])
+        msg = '{0.author.mention} option was removed '.format(message) + cmdParts[1]
+        await message.channel.send(msg)
+
+    elif message.content.startswith('!listOptions'):
+        options = WebScrape.getOptions()
+        msg = ''
+        if options == 'There are no current options setup use !setOptions to set a new option':
+            msg = options
+        else:
+            for opt in options:
+                msg = msg + opt + '\n'
+        await message.channel.send(msg)
+
+    elif message.content.startswith('!help'):
+        msg = 'Remember to seperate each part of the command with a space\n'
+        msg = msg + '!setOption <link to screener> <name of option (one word)>\n'
+        msg = msg + '!removeOption <name of option>\n'
+        msg = msg + '!listOptions\n'
+        msg = msg + '!tickers <name of option>\n'
         await message.channel.send(msg)
 
 @client.event
--- WebScrape.py
+++ WebScrape.py
@@ -6,6 +6,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 
+driver = None
+optionsDict = {}
+
 # function to ensure all key data fields have a value
 def validate_field(field):
     # if field is present pass if field:
@@ -16,23 +19,68 @@ def validate_field(field):
         field = 'No results'
     return field
 
-# specifies the path to the chromedriver.exe
-driver = webdriver.Chrome('C:/Users/Joey/Downloads/Install Files/chromedriver_win32/chromedriver.exe')
+def setupDriver():
+    global optionsDict
+    # load options from csv file
+    
+    with open('options.csv','r') as f:
+        line = f.readline()
+        while line:
+            parts = line.split(',', 1)
+            optionsDict[parts[0]] = parts[1]
+            line = f.readline()
 
-# driver.get method() will navigate to a page given by the URL address
-driver.get('https://finviz.com/screener.ashx?v=111')
+    global driver
+    # specifies the path to the chromedriver.exe
+    driver = webdriver.Chrome('C:/Users/Joey/Downloads/Install Files/chromedriver_win32/chromedriver.exe')
 
-sleep(1)
+    # driver.get method() will navigate to a page given by the URL address
+    driver.get('https://finviz.com/screener.ashx?v=111')
 
-oSelect = driver.find_element_by_id('fs_exch')
-options = [x for x in oSelect.find_elements_by_tag_name("option")]
-for option in options:
-    print(option.get_attribute("value") + ' ' + option.text)
+    sleep(1)
 
-print("")
+def printTickers(option):
+    global driver
+    global optionsDict
+    driver.get(optionsDict[option])
 
-tickerElements = driver.find_elements_by_class_name("screener-link-primary")
-for ticker in tickerElements:
-    print(ticker.text)
+    tickerElements = driver.find_elements_by_class_name("screener-link-primary")
+    textTickers = []
+    for ticker in tickerElements:
+        print(ticker.text)
+        textTickers.append(ticker.text + ' ')
 
-driver.quit()
+    return textTickers
+
+def setOption(link, name):
+    global optionsDict
+
+    with open('options.csv','a') as fd:
+        row = name +','+link
+        fd.writelines(row+'\n')
+
+    optionsDict[name] = link
+
+def removeOption(name):
+    global optionsDict
+    del optionsDict[name]
+    with open("options.csv", "r") as f:
+        lines = f.readlines()
+    with open("options.csv", "w") as f:
+        for line in lines:
+            print(line)
+            print(line.split(",",1))
+            if line.split(",",1)[0] != name:
+                f.write(line)
+
+def getOptions():
+    global optionsDict
+    if optionsDict.keys():
+        return optionsDict.keys()
+    else:
+        return "There are no current options setup use !setOptions to set a new option"
+
+def quitDriver():
+    driver.quit()
+
+setupDriver()
